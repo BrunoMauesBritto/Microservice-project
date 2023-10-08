2000b8d454d9bb939c5fe72e5f4b9fa29589343e
Bruno Britto <bruno_britto10@hotmail.com>
2023-10-08 12:00:53 +0200

    Login, JWT token

--- hr-oauth/src/main/java/com/bruno/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/bruno/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.bruno.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.bruno.hroauth.entities.User;
 import com.bruno.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	private static Logger Logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -23,6 +26,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		Logger.info("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			Logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		Logger.info("Email found: " + username);
 		return user;
 	}
 
